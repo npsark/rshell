9d68c81b125579f87e9df828ec993db22b7b45b2
Nat Sarkissian <nat@Nat-Debian.debian.local>
2014-11-16 21:10:58 -0800

    added basic piping and io redirection to rshell

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,78 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <signal.h>
 using namespace std;
 
 
 
 char** tokenize(string input, const char *delim);
-int parseForArgs(string input);
-void parseForLogic(string input);
+int parseForArgs(string input, int io = -1, string fileName = "");
+
+int parseForRedirection(string input);
+int parseForPipes(string input);
+int parseForOR(string input);
+void parseForAND(string input);
 void parseForCommands(string input);
-int runCommand(char **argv);
+
+int runCommand(char **argv, int io = -1, string fileName = "");
+string removeEdgeSpaces(string inString);
+void runPipe(string src, string dest){
+	int fd[2];
+	pipe(fd);
+
+	int pid = fork();
+
+	if(pid == 0){
+
+		//write to the pipe
+		if(-1 == dup2(fd[1],1)){//make stdout the write end of the pipe 
+		      perror("dup2()");
+		}
+
+		if(-1 == close(fd[0])){//close the read end of the pipe because we're not doing anything with it right now
+		      perror("close()");
+		}
+
+		char **toks = tokenize(src, " ");
+		execvp(toks[0], toks);
+		
+		exit(1);
+	}else{
+		wait(NULL);
+
+		//read end of the pipe
+		int savestdin;
+		if(-1 == (savestdin = dup(0))){//need to restore later or infinite loop
+		      perror("dup()");
+		}
+		if(-1 == dup2(fd[0],0)){//make stdin the read end of the pipe 
+		      perror("dup2()");
+		}
+		if(-1 == close(fd[1])){//close the write end of the pipe because we're not doing anything with it right now
+		      perror("close()");
+		}
+
+		parseForPipes(removeEdgeSpaces(dest));
+
+		dup2(savestdin,0);
+
+	}
+
+	//parseForRedirection(src);
+	//parseForPipes(dest);
+
+
+}
+
+
 
 int main(){
+
+
+
 
 	char *hostname = new char[50];
 	if(gethostname(hostname, 50) == -1){
@@ -57,6 +117,23 @@ int main(){
 
 }
 
+
+string removeEdgeSpaces(string inString){
+
+	string inStr = inString;
+	while(inStr[0] == ' '){
+		inStr.erase(inStr.begin());
+	}
+
+	while( *(inStr.end()-1) == ' '){
+		inStr.erase( inStr.end()-1 );
+	}
+
+	return inStr;
+}
+
+
+
 char** tokenize(string input, const char *delim){
 	
 	char *input_cstr = new char[input.length()+1];
@@ -94,13 +171,13 @@ char** tokenize(string input, const char *delim){
 }
 
 
-int parseForArgs(string input){
+int parseForArgs(string input, int io, string fileName){
 	
 	
 	//get tokens from input in char* array
 	char **toks = tokenize(input, " ");
 
-	int prg = runCommand(toks);	
+	int prg = runCommand(toks, io, removeEdgeSpaces(fileName));	
 
 	//clear the toks array
 	uint i=0;
@@ -120,28 +197,207 @@ int parseForArgs(string input){
 }
 
 
-void parseForLogic(string input){
+
+
+
+int parseForRedirection(string input){
 
 	uint i=0;
 
-	char **toks = tokenize(input, "&|");
+	char **toks = tokenize(input, "<>");
+	while(toks[i]){
+		i++;
+	}
+	
+
+	bool inputRedir = (input.find("<") != string::npos);
+
+
+	int success;
+
+
+
+	if(i == 2){		
+
+		//toks[0] will be the command with args
+		//toks[1] will be the file from which the input will come
+	
+		
+		//parse each bit between pipes(|).
+		if(inputRedir){
+			success = parseForArgs(toks[0], 0, string(toks[1]));		
+		}else{
+			success = parseForArgs(toks[0], 1, string(toks[1]));
+		}
+
+
+	}else{
+		success = parseForArgs(toks[0]);
+	}
+
+	i=0;
+	while(toks[i]){
+		delete toks[i];	
+		i++;
+	}
+	delete toks;
+	return success;
+
+}
+
+
+
+
+
+
+
+int parseForPipes(string input){
+
+	uint i=0;
+
+	char **toks = tokenize(input, "|");
+	while(toks[i]){
+		//cout << "toks" << "[" << i << "]: " << toks[i] << endl;
+		i++;
+	}
+
+	int success;
+
+	//cout << "SIZE: " << i << endl;
+	if(i >= 2){
+		
+		int res = input.find("|");
+
+		runPipe( string(toks[0]), input.substr(res+1)); 
+	}else{
+		i=0;
+		
+		while(toks[i]){
+			success = parseForRedirection(toks[i]);
+		
+			i++;
+		}
+
+
+	}
+
+	//clean up
+	i=0;
+	while(toks[i]){
+		delete toks[i];	
+		i++;
+	}
+	delete toks;
+	return success;
+
+}
+
+
+
+
+
+
+
+int parseForOR(string input){
+
+	int success;
+
+	bool orStatement = (input.find("||") != string::npos);
+	
+	int orLoc = 0;
+	bool a = false;
+	if(orStatement){
+
+		vector<string> toksV;
+		while(  !a   ){
+			int old = orLoc ;
+			a = (orLoc = input.find("||", orLoc + 1)) == -1;
+			if(old == 0){
+				toksV.push_back( removeEdgeSpaces(input.substr(old, orLoc - old)) );
+			}else{
+				toksV.push_back( removeEdgeSpaces(input.substr(old + 2, orLoc - old - 2)) );
+			}
+
+		}
+
+		for(uint l=0; l<toksV.size(); l++){
+			success = parseForOR(toksV[l]);
+
+			if( orStatement ){
+				if(success == 0){
+					return 0;
+				}
+			}
+		}
+
+		//int found = input.find("||");
+		//cout << input.substr(0, found) << endl;
+		//parseForOR(removeEdgeSpaces(input.substr(found + 2, input.length()-found - 2)));
+
+
+
+
+
+		/*char **toks = tokenize(input, "|");
+		i=0;
+
+	
+
+		while(toks[i]){
+
+			success = parseForPipes(toks[i]);
+		
+			if( orStatement ){
+				if(success == 0){
+					return 0;
+				}
+			}
+
+			i++;
+		}
+
+		//clean up
+		i=0;
+		while(toks[i]){
+			delete toks[i];	
+			i++;
+		}
+		delete toks;*/
+	
+	}else{
+
+		success = parseForPipes(input);		
+
+	}
+	
+	return success;
+
+}
+
+
+
+
+
+void parseForAND(string input){
+
+	uint i=0;
+
+	char **toks = tokenize(input, "&");
 	i=0;
 
 	//bool andStatement = (input.find("&&") != string::npos);
-	bool orStatement = (input.find("||") != string::npos);
+	bool andStatement = (input.find("&&") != string::npos);
 
 
 	while(toks[i]){
-		//parse each bit between | and &
-		int success = parseForArgs(toks[i]);
+		//cout << "AND: " << toks[i] << endl;
 
+		//parse each bit between | and &
+		//int success = parseForArgs(toks[i]);
+		int success = parseForOR(toks[i]);
 		
 	
-		if( orStatement ){
-			if(success == 0){
-				return;
-			}
-		}else if( !orStatement){
+		if( andStatement ){
 			if(success != 0){
 				return;
 			}
@@ -151,9 +407,9 @@ void parseForLogic(string input){
 	}
 	
 
+	//clean up
 	i=0;
 	while(toks[i]){
-		//parse each bit between semicolons
 		delete toks[i];	
 		i++;
 	}
@@ -168,13 +424,14 @@ void parseForCommands(string input){
 	uint i=0;
 	while(toks[i]){
 		//parse each bit between semicolons
-		parseForLogic(toks[i]);	
+		parseForAND(toks[i]);	
 		i++;
 	}
 	
+
+	//clean up
 	i=0;
 	while(toks[i]){
-		//parse each bit between semicolons
 		delete toks[i];	
 		i++;
 	}
@@ -183,31 +440,94 @@ void parseForCommands(string input){
 }
 
 
-int runCommand(char **argv){
+int runCommand(char **argv, int io, string fileName){
+
+
 
 	int status = 0;
+
+
+
 
 	int pid = fork();
 	if(pid == -1){
 
-		perror("fork() had an error.\n");
+		perror("fork()");
 		exit(1);
 
 	}else if(pid == 0){//child
 
+
+		//write to the pipe
+		/*if(-1 == dup2(fd[1],1)){//make stdout the write end of the pipe 
+		      perror("There was an error with dup2. ");
+		}
+		if(-1 == close(fd[0])){//close the read end of the pipe because we're not doing anything with it right now
+		      perror("There was an error with close. ");
+		}*/
+
+
+		if (io == 0){
+			int fd0 = open(fileName.c_str(), O_RDONLY, 0);
+			if(fd0 == -1){
+				perror("open");
+			}else{
+				if(dup2(fd0, STDIN_FILENO) == -1){
+					perror("dup2");
+				}
+				if(close(fd0) == -1){
+					perror("close");
+				}
+			}
+		}else if (io == 1){
+			int fd1 = creat(fileName.c_str(), 0644);
+			if(fd1 == -1){	
+				perror("creat");
+			}else{
+				if(dup2(fd1, STDOUT_FILENO) == -1){
+					perror("dup2");
+				}
+				if(close(fd1) == -1){
+					perror("close");
+				}
+			}
+		}
+
+
+
+
+
 		if(execvp(argv[0], argv) == -1){
-			perror("execvp() had an error.\n");
+			perror("execvp()");
 		}
 		
 		exit(1);
 
 	}else if(pid > 0){//parent
+
+
+
+		//read end of the pipe
+		/*int savestdin;
+		if(-1 == (savestdin = dup(0))){//need to restore later or infinite loop
+		      perror("There is an error with dup. ");
+		}
+		if(-1 == dup2(fd[0],0)){//make stdin the read end of the pipe 
+		      perror("There was an error with dup2. ");
+		}
+		if(-1 == close(fd[1])){//close the write end of the pipe because we're not doing anything with it right now
+		      perror("There was an error with close. ");
+		}*/
+
+
+
 		
 		if(wait(&status) == -1){
-			perror("wait() had an error.\n");
+			perror("wait()");
 		}
 
 	}
+
 	
 	return status;
 
